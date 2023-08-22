13f56454df73e5663ae23e4dfc29b50304dc8768
edgentai <93779481+edgentai@users.noreply.github.com>
2023-08-23 01:03:51 +0530

    Update reply.py

--- src/grievance_handler/reply.py
+++ src/grievance_handler/reply.py
@@ -10,27 +10,27 @@ access_token_secret = "W9t1PjPjD6PK40Ij6pa7pbrWwO5Yq27JiMTRQ2WIjOPwC"
 
 def tweet_reply(tweet_id, message):
   
-  payload = {"text": message, "reply": {"in_reply_to_tweet_id": tweet_id}}
-
-  # Make the request
-  oauth = OAuth1Session(
-      consumer_key,
-      client_secret=consumer_secret,
-      resource_owner_key=access_token,
-      resource_owner_secret=access_token_secret,
-  )
+    payload = {"text": message, "reply": {"in_reply_to_tweet_id": tweet_id}}
   
-  # Making the request
-  response = oauth.post(
-      "https://api.twitter.com/2/tweets",
-      json=payload,
-  )
-  
-  if response.status_code != 201:
-      raise Exception(
-          "Request returned an error: {} {}".format(response.status_code, response.text)
-      )
-  
-  print("Response code: {}".format(response.status_code))
+    # Make the request
+    oauth = OAuth1Session(
+        consumer_key,
+        client_secret=consumer_secret,
+        resource_owner_key=access_token,
+        resource_owner_secret=access_token_secret,
+    )
+    
+    # Making the request
+    response = oauth.post(
+        "https://api.twitter.com/2/tweets",
+        json=payload,
+    )
+    
+    if response.status_code != 201:
+        raise Exception(
+            "Request returned an error: {} {}".format(response.status_code, response.text)
+        )
+    
+    print("Response code: {}".format(response.status_code))
   
   
